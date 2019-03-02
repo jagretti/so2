6321c6eb969d661e3aff5f6f363bfe7fd4bd8121
jagretti <jose.agretti@gmail.com>
2019-03-02 10:30:21 -0300

    Arreglado test/ se puede hacer programas de usuario

--- trunk/code/test/install_cross_gcc.sh
+++ trunk/code/test/install_cross_gcc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tar xvzf gcc-mips.tar.gz
+tar xf gcc-mips.tar.xz
 mkdir $PWD/lib
 ln -s $PWD/mips-dec-ultrix42 $PWD/lib/gcc-lib
 echo -e "\n\nAhora debe reemplazar en test/Makefile la variable GCCDIR para que apunte a $PWD/mips-dec-ultrix42/mips- \n\n"
